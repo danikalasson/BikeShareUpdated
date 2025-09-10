16a076910d207ac6599ecc769f9799ec037c33df
danikalasson <145517660+danikalasson@users.noreply.github.com>
2025-09-09 19:58:05 -0600

    Complete EDA
    Made plots and completed EDA

--- BikeShare.R
+++ BikeShare.R
@@ -1,2 +1,18 @@
 library(tidyverse)
 library(tidymodels)
+library(vroom)
+library(ggplot2)
+library(DataExplorer)
+
+#Load in and clean data
+training_data <- vroom("C:/Users/lasso/OneDrive/Documents/Fall 2025/Stat 348/BikeShareUpdated/bike-sharing-demand/train.csv")
+training_data$weather <- as.factor(training_data$weather)
+
+#EDA
+plot_correlation(training_data)
+
+#Plots
+scatter_humidity <- ggplot(data = training_data, aes(x= humidity, y=count)) +
+  geom_point()
+barplot_weather <- ggplot(data = training_data, aes(x= humidity, y=count)) +
+  geom_bar()
